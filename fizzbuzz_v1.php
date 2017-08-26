939f853bae742bdb4121db1ea74c6c7961276108
Garrett Griess <github@thebluepig.com>
2017-08-26 17:55:26 -0500

    Update fizzbuzz_v1.php

--- fizzbuzz_v1.php
+++ fizzbuzz_v1.php
@@ -1,22 +1,34 @@
 <?
 /*
-	Garrett's First FizzBuzz Script Attempt - Version 1.0
+	Garrett's First FizzBuzz Script Attempt - Version 1.01
 
 	History Log:
+		Version 1.01
+		- Updated History Log for optimal readability.
+		- Wished I wouldn't have started a version history log.
+		
 		Version 1.0
-		 - Tested and ready for market. Created list of Future Plans. Dang it!
+		- Tested and ready for market.
+		- Created list of Future Plans. 
+		- Dang it!
 		 
 		Version 0.04
-		 - Changed $i to start at 1, Changed += to .= (was thinking of JS,) and fixed spelling mistake in comments. Dang it!
+		- Changed $i to start at 1
+		- Changed += to .= (was thinking of JS)
+		- Fixed spelling mistake in comments.
+		- Dang it!
 		 
 		Version 0.03
-		 - Added parentheses to "if !$num" line. Dang it!
+		- Added parentheses to "if !$num" line.
+		- Dang it!
 		
 		Version 0.02
-		 - Removed semicolon from for loop. Got carried away with the semicolons. Dang it!
+		- Removed semicolon from for loop. (Got carried away with the semicolons.)
+		- Dang it!
 
 	Future Plans:
-		- Give up on programming forever and apply at nearest fast food restaurant.
+		- Give up on programming forever.
+		- Apply at nearest fast food restaurant.
 */
 
 for ($i=1;$i<=100;$i++) {
